e8c684acdeeee9dd4c7c9f0427dcc0e0ce0c3aa6
Nilu patel <nilusingh945164@gmail.com>
2024-03-20 19:02:39 +0530

    js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 
  //// given output but not give the value of x
-getname();
-console.log(x);   //x is undefind
+// getname();
+// console.log(x);   //x is undefind
 
 var x=9;
-function getname(){
-    console.log("namste javascripct");
-}
+// function getname(){
+//     console.log("namste javascripct");
+// }
 
   //given output and value of x
-// getname();
-// console.log(x);
+getname();
+console.log(x);
+console.log(getname);
+
+
+
+var getname = () =>{
+    console.log("namste javascripct");
+}
