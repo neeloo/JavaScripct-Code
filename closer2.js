ee55fa88b8c1d2dd689e7eec70412959f23565e3
Nilu patel <nilusingh945164@gmail.com>
2024-04-11 10:46:48 +0530

    run

--- closer2.js
+++ closer2.js
@@ -1,3 +1,4 @@
+/*
 function cc(f, s) {
     let a = f;
     let y = s;
@@ -13,3 +14,26 @@ const c1 = cc(1, 6);
 const c2 = cc(9, 9);
 console.log(c1(), c1(), c1());
 console.log(c2(), c2(), c2());
+
+*/
+
+function a() {
+    let x = 10;
+    console.log(x);
+    function b() {
+        let x = 20;
+          console.log(x);
+        function c() {
+            let x = 30;
+             console.log(x)
+            function d() {
+                let x = 40;
+                console.log(x)
+
+            }return d;
+
+        }return c;
+
+    }return b;
+
+} a()()()();
