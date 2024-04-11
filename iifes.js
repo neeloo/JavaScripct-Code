6a5d31e0076e09d4a766c0f8b966f22493927713
Nilu patel <nilusingh945164@gmail.com>
2024-04-11 13:26:07 +0530

    run

---
+++ iifes.js
@@ -0,0 +1,13 @@
+//iifes- stand for immediately invoked function expresstion
+ 
+// write like that example
+(function printiife(){
+    console.log("hellow")
+})
+();
+
+//no need to write a function name
+(function (){
+    console.log("Hellow iifes")
+})
+();
