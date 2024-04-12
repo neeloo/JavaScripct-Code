7b836620e05a00032631ee36614ad38879e93195
Nilu patel <nilusingh945164@gmail.com>
2024-04-12 13:52:29 +0530

    in

--- object.inheritance.js
+++ object.inheritance.js
@@ -1,8 +1,13 @@
-let a={p:10};
+let a = { p: 10 };
+let c = { k: 34 };
 
-let b=Object.create(a);
-console.log(a,b);        // o/p =>{p:10}{}
-console.log(b.p)         //10
+let b = Object.create(a);
+console.log(a, b);                  // o/p =>{p:10}{}
+console.log(b.p)                    //10
 
-console.log(a.isPrototypeOf(b))   //true
+console.log(a.isPrototypeOf(b))     //true
 console.log(b.__proto__ == a)       //true
+console.log(a.isPrototypeOf(c))     //false
+
+console.log(a == b)                //false (beacuse b is inheritance of a  not equal of a)
+console.log(a === b)              //false (strong check)
