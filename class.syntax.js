52a86043fbe61f706ba47e3d1db37ec8a34cc5ca
Nilu patel <nilusingh945164@gmail.com>
2024-04-12 17:37:17 +0530

    /

--- class.syntax.js
+++ class.syntax.js
@@ -3,6 +3,24 @@ class person{
         this.name=name;
         this.age=age;
     }
+    isAdult(){
+        return this.age>=18;
+    }
+    //setter and getter
+    get firstname(){
+        return this.name.split('  ')[0];
+    }
+    set firstname(value){
+        const newn= this.name.split('')
+        newn[0]=value;
+        this.name=newn.join('');
+
+    }
 }
-const p1=new person("mn",7);
+const p1=new person(" nilu singh",7);
+console.log(p1)
+
+console.log(p1.firstname);
+
+p1.firstname='smir singh'
 console.log(p1)
