5bcdc5efbfa896600ee371d53ca79cbc8756b658
Nilu patel <nilusingh945164@gmail.com>
2024-04-14 14:29:50 +0530

    .

--- observalbble.js
+++ observalbble.js
@@ -3,23 +3,40 @@ const inpdelta = document.getElementById("delta");
 
 const btncounter = document.getElementById("btncounter");
 
-const divcounter = document.getElementById("counter");
+const divcounter = document.getElementById("counters");
+
+window.counters = [];
+
+btncounter.onclick = function () {
+    const counter = new Counter(inpinit.value, inpdelta.value);
+    divcounter.appendChild(counter.el)
+    counter.el.onclick = function () {
+        counter.count();
+    }
+    window.counters.push(counter);
+}
 
 
 class Counter {
     constructor(init, delta) {
-        this.init = init;
-        this.delta = delta;
+        this.init = Number(init);
+        this.delta = Number(delta);
+        ///create element
+        this.el = document.createElement('div');
+        this.el.innerText = `Count = ${init}`
+
     }
     get value() {
         return this.init;
     }
     set value(x) {
+        this.el.innerText = `Count=${x}`
         this.init = x;
 
     }
     count() {
-        this.value += this.delta;
+        this.value = this.value + this.delta;
+
     }
 
 }
