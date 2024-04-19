70965e7ebbf8e4f61a67d96e9da6bc2dd436f0a1
Nilu patel <nilusingh945164@gmail.com>
2024-04-19 14:15:34 +0530

    spread

--- JAVASCRIPCT/spread_rest_default.js
+++ JAVASCRIPCT/spread_rest_default.js
@@ -5,10 +5,10 @@
 // fn("hi","hellow","hey");   //hi hellow hey
 // fn ("hi","hellow");       //hi hellow undefined
 
-function fn(p1,p2,p3="default"){
-    console.log(p1,p2,p3);
-}
-fn("hi","hellow");      //output-hi hellow default
+// function fn(p1,p2,p3="default"){
+//     console.log(p1,p2,p3);
+// }
+// fn("hi","hellow");      //output-hi hellow default
      
 
 
@@ -21,12 +21,21 @@ fn("hi","hellow");      //output-hi hellow default
 // console.log(arr);    ///o/p->>>[ 1, 2, 200, 4, 100 ]
 
 // use spread
-let arr=[1,2,3,4,5];
+// let arr=[1,2,3,4,5];
+// let arr2=[...arr];
+// arr2.pop();
+// arr2.push(100);
+// arr2[2]=200;
+// console.log(arr);   // output-[ 1, 2, 3, 4, 5 ]
+
+
+
+let arr=[1,2,[3,4],5];
 let arr2=[...arr];
 arr2.pop();
 arr2.push(100);
 arr2[2]=200;
-console.log(arr);   // output-[ 1, 2, 3, 4, 5 ]
+console.log(arr);
 
 
 
