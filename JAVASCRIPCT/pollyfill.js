4551fefbbb28eee1084ac44eff7afb551e8a77f8
Nilu patel <nilusingh945164@gmail.com>
2024-04-20 21:47:39 +0530

    ..

--- JAVASCRIPCT/pollyfill.js
+++ JAVASCRIPCT/pollyfill.js
@@ -1,15 +1,18 @@
-let cap={
-    name:"steave",
-    team:"cap",
-    pteam:function(){
-        console.log(`hey ${this.name} and team ${this.team}` );
-    }
-}
-let ironman={
-    name:"tony",
-    team:"iron man"
-}
+// let cap = {
+//     name: "steave",
+//     team: "cap",
+//     pteam: function () {
+//         console.log(`hey ${this.name} and team ${this.team}`);
+//     }
+// }
+// let ironman = {
+//     name: "tony",
+//     team: "iron man"
+// }
+
+
 //build pollyfills
+
 // Function.prototype.mycall=function(){
 //     console.log("hi i am invoked");
 // }
@@ -21,12 +24,54 @@ let ironman={
 // fn.mycall();
 
 
-Function.prototype.mycall=function(){
-    //function is send as a this
-    let req=this;
-    //add that function to obj
-    ironman.req=req;
-   // with that obj your function
-    ironman.req();
+// Function.prototype.mycall = function () {
+//     //function is send as a this
+//     let req = this;
+//     //add that function to obj
+//     ironman.req = req;
+//     // with that obj your function
+//     ironman.req();
+// }
+// cap.pteam.mycall(ironman);
+// // or same as like that
+// cap.pteam.call(ironman);
+
+
+
+// pollyfill of call method*******************
+let cap = {
+    name: "steave",
+    team: "cap",
+    pteam: function (m1, m2) {
+        console.log(`hey ${this.name} and team ${this.team} and member is ${m1} and ${m2}`);
+    }
 }
-cap.pteam.mycall(ironman);
+let ironman = {
+    name: "tony",
+    team: "iron man"
+}
+Function.prototype.mycall = function (reqobj, ...arg) {
+    //function is send as a this
+    let req = this;
+    //add that function to obj
+    reqobj.req = req;
+    // with that obj your function
+    reqobj.req(...arg);
+    // deleted tha add function
+    //delete reqobj.req;
+}
+// use of pollyfil()***************
+let pteamf = cap.pteam;
+pteamf.mycall(ironman, "loki", "thor");
+
+//work same as write like that
+// call()*************
+cap.pteam.call(ironman, "subham", "madhu");
+
+// apply()*************
+cap.pteam.apply(ironman, ["samir", "sma"])
+
+// bind()************
+let pt = cap.pteam.bind(ironman);
+pt("asma", "kirti");
+pt("aakaskh", "anusaka");
