9129b395e88824cc7c1d13b8dc4c3d1aa6fa0b30
Nilu patel <nilusingh945164@gmail.com>
2024-04-22 16:52:44 +0530

    .

--- JAVASCRIPCT/Array.js
+++ JAVASCRIPCT/Array.js
@@ -1,8 +1,21 @@
-let arr=[1,2,3,4,5,6,7,8];
-// slice()  = from start index to end index
-let slicearr=arr.slice(2,4);
-console.log(slicearr);
+// let arr=[1,2,3,4,5,6,7,8];
+// // slice()  = from start index to end index
+// let slicearr=arr.slice(2,4);
+// console.log(slicearr);
 
-// splice()  fron start index and delete count
-let splicearr=arr.splice(2,3);
-console.log(arr,splicearr);
+// // splice()  fron start index and delete count
+// let splicearr=arr.splice(2,3);
+// console.log(arr,splicearr);
+
+
+// let arr2=[100,22,33];
+// let c=arr.concat(arr2);
+// console.log(c);
+
+
+// splite and join
+let str="hi i am alexa";
+let spiltstr=str.split(" ");
+let joinstr=spiltstr.join("+");
+console.log(joinstr);
+console.log(spiltstr);
---
+++ JAVASCRIPCT/function.js
@@ -0,0 +1,18 @@
+// function fn(){
+//     console.log("i am fn")
+// }
+// fn. count=0;
+
+// fn. showfn=function(){
+//     console.log("count",fn.count);
+
+// }
+// fn.showfn();
+
+// variable- pass a parameter
+function fn(param){
+    console.log(param);
+}
+fn("hello");
+fn([1,2,3,4]);
+fn({name:"nilu"})
