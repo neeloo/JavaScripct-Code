cc089633331c0efd524cd3cf6fcaf04fcfd7b91e
Nilu patel <nilusingh945164@gmail.com>
2024-04-22 16:04:12 +0530

    .

---
+++ JAVASCRIPCT/Array.js
@@ -0,0 +1,8 @@
+let arr=[1,2,3,4,5,6,7,8];
+// slice()  = from start index to end index
+let slicearr=arr.slice(2,4);
+console.log(slicearr);
+
+// splice()  fron start index and delete count
+let splicearr=arr.splice(2,3);
+console.log(arr,splicearr);
