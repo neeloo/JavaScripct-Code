a14848aa63f6a212c159f0e5f38bdd4f053dfc43
Nilu patel <nilusingh945164@gmail.com>
2024-04-24 00:38:34 +0530

    .

--- JAVASCRIPCT/closure.js
+++ JAVASCRIPCT/closure.js
@@ -13,16 +13,134 @@
 
 
 
-function createcount(a,b){
-    function count(){
-        a=a+b;
-        return a;
+// function createcount(a,b){
+//     function count(){
+//         a=a+b;
+//         return a;
+//     }
+//     return count;
+// }
+// let c=createcount(10,5);
+// let d=createcount(3,2);
+// console.log(c());
+// console.log(d());
+// console.log(c());
+// console.log(d());
+
+
+/**
+ * nested closure-
+ */
+
+// function getname(firstname) {
+//     console.log("i got first name");
+//     return function getlastname(lastname) {
+//         console.log("i got lastname");
+//         return function geeter() {
+//             console.log(`Hi i am ${firstname} ${lastname}`)
+//         }
+//     }
+// }
+// let c = getname("Nilu");
+// // console.log(c)
+// let d = c("Singh");
+// // console.log(d);
+// d();
+
+// // short code like this
+// getname("Swatnatra Praksha")("Verma")();
+
+
+
+
+/** 
+ * application of closure
+ * 1-currying- talking one input at a time and use the input later
+ */
+
+
+// function  f1(firstname){
+//     console.log("11");
+//     return function f2(lastname){
+//         console.log("12");
+//         return function f3(){
+//             console.log(` hi i am ${firstname} ${lastname}`);
+//         }
+//     }
+// }
+// const k=f1("anu");
+// const h=k("patel");
+// h();
+
+
+// ashynchrounos code can not run without clouser
+
+// console.log("Befour");
+// function fn(){
+//     console.log("i will explod");
+// }
+// setTimeout(fn,2000);
+// console.log("after")
+
+
+// Question 
+// function outer() {
+//     let arr = [];
+//     for (var i = 0; i < 3; i++) {
+//         arr.push(function f() { console.log(i) });
+//     }
+//     return arr;
+// }
+// let arr = outer();
+// arr[0](); //o/p->3
+// arr[1]();  //o/p->3
+// arr[2]();   //o/p->3
+
+// q-2
+// function out() {
+//     let arr2 = [];
+//     let i = 0;
+//     for (i = 0; i < 3; i++) {
+//         arr2.push(function f2() { console.log(i) });
+//     }
+//     return arr2;
+// }
+// let arr2 = out();
+// arr2[0]();   //o/p->3
+// arr2[1]();    //o/p->3
+// arr2[2]();   //o/p->3
+
+// q-3
+
+// function oute(){
+//     let a=[];
+//     for(let i=0;i<3;i++){
+//         a.push(function f3(){console.log(i)});
+//     }
+//     return a;
+// }
+
+// let a=oute();
+// a[0]();       //o/p->0
+// a[1]();        //o/p->1
+// a[2]();         //o/p->2
+
+
+
+
+
+// infinite curry
+function counter(a) {
+    let count = 0;
+    if (a == 0) { count++; return count; }
+    else {
+        return function inner(a) {
+            count++;
+            if (a == 0) { return count; }
+            else { return inner; }
+        }
     }
-    return count;
 }
-let c=createcount(10,5);
-let d=createcount(3,2);
-console.log(c());
-console.log(d());
-console.log(c());
-console.log(d());
+console.log(counter(0));  //1
+console.log(counter()(0)) //2
+console.log(counter()()(0))  //3
---
+++ JAVASCRIPCT/lexial-scope.js
@@ -0,0 +1,26 @@
+/**
+ * outer scope  is lexiacally scope
+ * outer scope always define function definition
+ */
+
+// Q_1
+// let n=20;    //outer scope
+// function fn(){
+//      let n=10;    // this is local scope if local scope is not present the call outer scope
+//     console.log(n);
+// }fn();
+
+// outer scope always define function definition
+
+// let a=10;
+// function f(){        // function definition
+//     console.log(a);
+// }
+// function f2(){
+//     let a=90;
+//     f();         //function call
+//     console.log(a);
+// }
+// f2();
+
+
