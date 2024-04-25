adf3f5d8f447c67a0286b19ef6df81dba6dbd4a8
Nilu patel <nilusingh945164@gmail.com>
2024-04-26 00:08:31 +0530

    .

--- JAVASCRIPCT/time.js
+++ JAVASCRIPCT/time.js
@@ -9,14 +9,36 @@
 
 
 // cleartimeout()
-console.log("before")
-function cb1() {
-    console.log("i  will explosive")
+// console.log("before")
+// function cb1() {
+//     console.log("i  will explosive")
+// }
+// const id = setTimeout(cb1, 3000);
+// function cb2() {
+//     console.log("cancelling")
+//     clearTimeout(id)
+// }
+// setTimeout(cb2, 2000)
+// console.log("after")
+
+
+// setInterval()
+// console.log("before");
+// function cb(){
+//     console.log("interval call")
+// }
+// setInterval(cb,2000);
+// console.log("after")
+
+// clearInterval()
+console.log("before");
+function cb(){
+    console.log("interval call")
 }
-const id = setTimeout(cb1, 3000);
-function cb2() {
+const id=setInterval(cb,2000);
+function cb2(){
     console.log("cancelling")
-    clearTimeout(id)
+    clearInterval(id);
 }
-setTimeout(cb2, 2000)
+setTimeout(cb2,3000)
 console.log("after")
