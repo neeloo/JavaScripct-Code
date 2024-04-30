b7fb6860c623b9bd4e302cf3c93e2d5a4f1160d4
Nilu patel <nilusingh945164@gmail.com>
2024-04-30 20:42:45 +0530

    .

--- JAVASCRIPCT/time.js
+++ JAVASCRIPCT/time.js
@@ -1,47 +1,47 @@
 // /**setTimeout() and cleartimeout() */
-// // console.log("before")
-// // function fn(){
-// //     console.log("i  will explosive")
-// // }
-// // setTimeout(fn,2000);
-// // console.log("after")
+// console.log("before")
+// function fn(){
+//     console.log("i  will explosive")
+// }
+// setTimeout(fn,2000);
+// console.log("after")
 
 
 
 // // cleartimeout()*********
-// // console.log("before")
-// // function cb1() {
-// //     console.log("i  will explosive")
-// // }
-// // const id = setTimeout(cb1, 3000);
-// // function cb2() {
-// //     console.log("cancelling")
-// //     clearTimeout(id)
-// // }
-// // setTimeout(cb2, 2000)
-// // console.log("after")
+// console.log("before")
+// function cb1() {
+//     console.log("i  will explosive")
+// }
+// const id = setTimeout(cb1, 3000);
+// function cb2() {
+//     console.log("cancelling")
+//     clearTimeout(id)
+// }
+// setTimeout(cb2, 2000)
+// console.log("after")
 
 
-// // setInterval()******************
-// // console.log("before");
-// // function cb(){
-// //     console.log("interval call")
-// // }
-// // setInterval(cb,2000);
-// // console.log("after")
+// // // setInterval()******************
+// console.log("before");
+// function cb(){
+//     console.log("interval call")
+// }
+// setInterval(cb,2000);
+// console.log("after")
 
 // // clearInterval()*****************
-// // console.log("before");
-// // function cb(){
-// //     console.log("interval call")
-// // }
-// // const id=setInterval(cb,2000);
-// // function cb2(){
-// //     console.log("cancelling")
-// //     clearInterval(id);
-// // }
-// // setTimeout(cb2,3000)
-// // console.log("after")
+// console.log("before");
+// function cb(){
+//     console.log("interval call")
+// }
+// const id=setInterval(cb,2000);
+// function cb2(){
+//     console.log("cancelling")
+//     clearInterval(id);
+// }
+// setTimeout(cb2,3000)
+//console.log("after")
 
 
 // // create pollyfill  of setinterval
@@ -97,7 +97,7 @@
 
 
 // reassign
-
+/*
 let c={
     appname:"scaler",
     database:{
@@ -111,3 +111,4 @@ c.temp="127.0.018";
 delete appname.database.pwd;
 c.appname="interviewbit.com";
 console.log(c)
+*/
